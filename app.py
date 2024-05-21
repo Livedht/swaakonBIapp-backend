842624007ae7cabdc860e9f7e6c216f5fcdc6880
Livedht <your.email@example.com>
2024-05-21 22:27:14 +0200

    Prepare app for Heroku deployment

--- app.py
+++ app.py
@@ -333,5 +333,6 @@ def home():
     else:
         return jsonify({'message': 'Ready for overlap checking'})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
